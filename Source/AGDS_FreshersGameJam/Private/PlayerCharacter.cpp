a8dc271a9c8c7481eb8c43ebff42233a419ed40e
Doshi <phoenix0244@gmail.com>
2024-09-20 17:15:47 +0100

    added basic movement component

--- Source/AGDS_FreshersGameJam/Private/PlayerCharacter.cpp
+++ Source/AGDS_FreshersGameJam/Private/PlayerCharacter.cpp
@@ -2,13 +2,15 @@
 
 
 #include "PlayerCharacter.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 
 // Sets default values
-APlayerCharacter::APlayerCharacter()
+APlayerCharacter::APlayerCharacter(): LookAction(nullptr), MoveAction(nullptr), JumpAction(nullptr),
+                                      MappingContext(nullptr)
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
@@ -30,5 +32,50 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	if (UEnhancedInputComponent* PlayerEnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		if (MoveAction) 
+		{
+			PlayerEnhancedInputComponent->BindAction(MoveAction,ETriggerEvent::Triggered,this , &APlayerCharacter::Move);
+		}
+		if (LookAction)
+		{
+			PlayerEnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Look);
+		}
+		if (JumpAction)
+		{
+			PlayerEnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
+			PlayerEnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+		}
+	}
+}
+
+void APlayerCharacter::PawnClientRestart()
+{
+	Super::PawnClientRestart();
+
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = Cast<APlayerController>(GetController())->GetLocalPlayer()->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+	{
+		Subsystem->ClearAllMappings();
+		Subsystem->AddMappingContext(MappingContext,0);
+	}
+}
+
+void APlayerCharacter::Move(const FInputActionValue& ActionValue)
+{
+	if (Controller != nullptr)
+	{
+		AddMovementInput(GetActorForwardVector(), ActionValue[0]);
+		AddMovementInput(GetActorRightVector(), ActionValue[1]);
+	}
+}
+
+void APlayerCharacter::Look(const FInputActionValue& ActionValue)
+{
+	if (Controller != nullptr)
+	{
+		AddControllerYawInput(ActionValue[0]);
+		AddControllerPitchInput(ActionValue[1]);
+	}
 }
 
